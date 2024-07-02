407f66f0102be986d0764b90ff0ace1933bf21ef
shfqmn <prof.shafiq@gmail.com>
2024-07-02 15:13:44 +0800

    Create intro.vue

---
+++ pages/intro.vue
@@ -0,0 +1,13 @@
+<script>
+definePageMeta({ layout: 'page' })
+</script>
+
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>
+      We are a group of powerlifting enthusiasts who are passionate about the
+      sport and want to promote it in Johor.
+    </p>
+  </div>
+</template>
