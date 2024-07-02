3804eaf9818dde29882662f89b17270dc4adebc1
shfqmn <prof.shafiq@gmail.com>
2024-07-02 15:36:03 +0800

    Update Navbar.vue

--- components/layouts/Page/Navbar.vue
+++ components/layouts/Page/Navbar.vue
@@ -15,19 +15,30 @@ const showDrawer = ref(false)
 </script>
 
 <template>
-  <header class="flex fixed backdrop-filter backdrop-blur-md top-0 z-40 w-full flex-none transition-colors duration-300 lg:z-50 border-b border-gray-950/10 dark:border-gray-50/[0.2] bg-white/[0.5] dark:bg-gray-950/[0.5] pb-3 pt-3">
+  <header
+    class="flex fixed backdrop-filter backdrop-blur-md top-0 z-40 w-full flex-none transition-colors duration-300 lg:z-50 border-b border-gray-950/10 dark:border-gray-50/[0.2] bg-white/[0.5] dark:bg-gray-950/[0.5] pb-3 pt-3"
+  >
     <!-- content -->
-    <div class="flex-1 flex items-center justify-between max-w-screen-2xl mx-auto px-4">
+    <div
+      class="flex-1 flex items-center justify-between max-w-screen-2xl mx-auto px-4"
+    >
       <!-- title -->
       <div>
         <slot name="title">
           <NuxtLink to="/" class="font-bold text-lg">
-            <Img src="/jap_logo.jpg" class="font-black text-xl font-mono mr-2 inline-block rounded-full logo" />
+            <Img
+              src="/jap_logo.jpg"
+              class="font-black text-xl font-mono mr-2 inline-block rounded-full logo"
+            />
           </NuxtLink>
         </slot>
       </div>
       <!-- menus -->
-      <div v-if="$screen.higherThan('md', $screen.current.value)" class="flex space-x-4 items-center" :class="{ 'divide-x divide-gray-500': menus.length > 0 }">
+      <div
+        v-if="$screen.higherThan('md', $screen.current.value)"
+        class="flex space-x-4 items-center"
+        :class="{ 'divide-x divide-gray-500': menus.length > 0 }"
+      >
         <div class="flex space-x-4 text-sm items-center">
           <!-- dynamic menus -->
           <template v-for="(item, i) in menus" :key="i">
@@ -44,9 +55,17 @@ const showDrawer = ref(false)
         </div>
       </div>
       <!-- drawer:btn -->
-      <div v-else class="flex space-x-4 items-center" :class="{ 'divide-x divide-gray-500': menus.length > 0 }">
+      <div
+        v-else
+        class="flex space-x-4 items-center"
+        :class="{ 'divide-x divide-gray-500': menus.length > 0 }"
+      >
         <div class="pl-4 flex space-x-3 text-xl">
-          <AwesomeLink v-if="awesome?.project?.links?.github" class="text-gray-400 hover:text-gray-100" @click.prevent="() => (showDrawer = !showDrawer)">
+          <AwesomeLink
+            v-if="awesome?.project?.links?.github"
+            class="text-gray-400 hover:text-gray-100"
+            @click.prevent="() => (showDrawer = !showDrawer)"
+          >
             <Icon name="heroicons:bars-3-bottom-right-20-solid" />
           </AwesomeLink>
         </div>
@@ -54,54 +73,95 @@ const showDrawer = ref(false)
     </div>
     <!-- misc -->
     <!-- drawer -->
-    <AwesomeActionSheet v-if="!$screen.higherThan('md', $screen.current.value) && showDrawer" @close="() => (showDrawer = false)">
+    <AwesomeActionSheet
+      v-if="!$screen.higherThan('md', $screen.current.value) && showDrawer"
+      @close="() => (showDrawer = false)"
+    >
       <AwesomeActionSheetGroup>
         <AwesomeActionSheetHeader>
           <AwesomeActionSheetHeaderTitle text="Menu" />
         </AwesomeActionSheetHeader>
         <!-- dynamic menus -->
         <AwesomeActionSheetItem>
-          <div class="flex flex-col text-sm items-center divide-y divide-gray-400 dark:divide-gray-700 text-center">
+          <div
+            class="flex flex-col text-sm items-center divide-y divide-gray-400 dark:divide-gray-700 text-center"
+          >
             <template v-for="(item, i) in menus">
               <template v-if="item?.type === 'link'">
-                <NuxtLink :key="i" :to="parseMenuRoute(item.to)" #="{ isActive }" class="w-full py-2">
-                  <span :class="{
-                    'text-gray-900 dark:text-gray-100 font-bold': isActive,
-                    'text-gray-700 dark:text-gray-300': !isActive,
-                  }">{{ parseMenuTitle(item?.title) }}</span>
+                <NuxtLink
+                  :key="i"
+                  :to="parseMenuRoute(item.to)"
+                  #="{ isActive }"
+                  class="w-full py-2"
+                >
+                  <span
+                    :class="{
+                      'text-gray-900 dark:text-gray-100 font-bold': isActive,
+                      'text-gray-700 dark:text-gray-300': !isActive,
+                    }"
+                    >{{ parseMenuTitle(item?.title) }}</span
+                  >
                 </NuxtLink>
               </template>
               <template v-if="item?.type === 'button'">
-                <AwesomeButton :key="i" :text="parseMenuTitle(item?.title)" size="sm" :to="parseMenuRoute(item.to)" class="w-full" />
+                <AwesomeButton
+                  :key="i"
+                  :text="parseMenuTitle(item?.title)"
+                  size="sm"
+                  :to="parseMenuRoute(item.to)"
+                  class="w-full"
+                />
               </template>
               <template v-if="item?.type === 'dropdown'">
                 <div :key="i" class="w-full">
                   <HeadlessDisclosure v-slot="{ open }">
-                    <HeadlessDisclosureButton :key="i" :class="[
-                      'text-gray-700 dark:text-gray-300 w-full py-2 flex items-center justify-center duration-300 transition-all',
-                      open ? 'font-bold' : '',
-                    ]">
-                      <span>{{
-                        parseMenuTitle(item?.title)
-                        }}</span>
-                      <Icon name="carbon:chevron-right" class="ml-1" :class="[
-                        open
-                          ? 'duration-300 transition-all transform rotate-90'
-                          : 'rotate-0',
-                      ]" />
+                    <HeadlessDisclosureButton
+                      :key="i"
+                      :class="[
+                        'text-gray-700 dark:text-gray-300 w-full py-2 flex items-center justify-center duration-300 transition-all',
+                        open ? 'font-bold' : '',
+                      ]"
+                    >
+                      <span>{{ parseMenuTitle(item?.title) }}</span>
+                      <Icon
+                        name="carbon:chevron-right"
+                        class="ml-1"
+                        :class="[
+                          open
+                            ? 'duration-300 transition-all transform rotate-90'
+                            : 'rotate-0',
+                        ]"
+                      />
                     </HeadlessDisclosureButton>
-                    <Transition enter-active-class="transition duration-100 ease-out" enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-out" leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+                    <Transition
+                      enter-active-class="transition duration-100 ease-out"
+                      enter-from-class="transform scale-95 opacity-0"
+                      enter-to-class="transform scale-100 opacity-100"
+                      leave-active-class="transition duration-75 ease-out"
+                      leave-from-class="transform scale-100 opacity-100"
+                      leave-to-class="transform scale-95 opacity-0"
+                    >
                       <HeadlessDisclosurePanel class="text-gray-500 pb-2">
-                        <template v-for="(child, j) in item?.children || []" :key="j">
-                          <NuxtLink :to="parseMenuRoute(child.to)" #="{ isActive }" class="w-full py-2">
-                            <span :class="[
-                              isActive
-                                ? 'text-gray-900 dark:text-gray-100 font-bold'
-                                : 'text-gray-700 dark:text-gray-300',
-                            ]">{{
-                              parseMenuTitle(child?.title)
-                              }}</span>
-                          </NuxtLink>
+                        <template
+                          v-for="(child, j) in item?.children || []"
+                          :key="j"
+                        >
+                          <div>
+                            <NuxtLink
+                              :to="parseMenuRoute(child.to)"
+                              #="{ isActive }"
+                              class="w-full py-2"
+                            >
+                              <span
+                                :class="[
+                                  isActive
+                                    ? 'text-gray-900 dark:text-gray-100 font-bold'
+                                    : 'text-gray-700 dark:text-gray-300',
+                                ]"
+                                >{{ parseMenuTitle(child?.title) }}</span
+                              >
+                            </NuxtLink>
+                          </div>
                         </template>
                       </HeadlessDisclosurePanel>
                     </Transition>
@@ -123,7 +183,6 @@ const showDrawer = ref(false)
     </AwesomeActionSheet>
   </header>
 </template>
-
 
 <style scoped>
 .logo {
