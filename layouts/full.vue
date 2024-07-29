00f8e5355cbe43c1ebc0b3a8b800a5392544f606
shfqmn <prof.shafiq@gmail.com>
2024-07-29 12:19:43 +0800

    Update full.vue

--- layouts/full.vue
+++ layouts/full.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="overflow-x-hidden">
     <LayoutPageNavbar class="h-[64px] max-h-[64px]" />
-    <div>
+    <div class="flex pt-[64px]" :style="{ 'min-height': 'var(--layout-page-content-min-height)' }">
       <slot />
     </div>
     <LayoutPageFooter class="h-[52px] md:h-[42px]" />
