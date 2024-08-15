d0633136b370ac49b8b8efd33581ff2a7aefcf94
shfqmn <prof.shafiq@gmail.com>
2024-08-16 05:00:16 +0800

    Update jpo-2023.vue

--- pages/events/jpo-2023.vue
+++ pages/events/jpo-2023.vue
@@ -2,6 +2,8 @@
   <LayoutPageWrapper>
     <LayoutPageSection>
       <div>
+        <div style="padding:177.78% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/999276852?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="jpo1"></iframe></div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
         <h1>Johor Powerlifting Open Volume 1 2023</h1>
         <div class="grid grid-cols sm:grid-cols-2 md:grid-cols-3 gap-4">
           <NuxtImg provider="cloudinary" src="/jpo2023/jpo2.jpg" class="object-contain w-50 h-auto image-zoomable" />
