e241fd7ab82ba02f29c075a5a489a3691dc9c6de
shafiq.aiman <prof.shafiq@gmail.com>
2024-12-17 16:51:40 +0800

    update style

--- pages/women-state-records.vue
+++ pages/women-state-records.vue
@@ -1,9 +1,9 @@
 <template>
   <LayoutPageWrapper>
     <LayoutPageSection>
-      <div>
-        <h1>Women's State Records</h1>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSUcSu0vVY9qSG0hV9gUeLd5NI4YeeyaiX15ZGZHP5S7j_yopg0vmcVzaDR1nIlocZj-0wp_I8xUrzE/pubhtml?gid=939188442&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      <h1>Women's State Records</h1>
+      <div class="iframe-container">
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSUcSu0vVY9qSG0hV9gUeLd5NI4YeeyaiX15ZGZHP5S7j_yopg0vmcVzaDR1nIlocZj-0wp_I8xUrzE/pubhtml?gid=939188442&amp;single=true&amp;widget=true&amp;headers=false" class="responsive-iframe"></iframe>
       </div>
     </LayoutPageSection>
   </LayoutPageWrapper>
@@ -30,5 +30,17 @@ p {
   font-size: 1.125rem;
   line-height: 1.75;
   margin-bottom: 1.5rem;
+}
+
+.iframe-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.responsive-iframe {
+  width: 100%;
+  height: 100vh;
+  border: none;
 }
 </style>
