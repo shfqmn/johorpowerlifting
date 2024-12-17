06cfc2341ee610f16f232e299fffd0f95a0d0f83
shafiq.aiman <prof.shafiq@gmail.com>
2024-12-17 16:57:39 +0800

    update register page

--- pages/register.vue
+++ pages/register.vue
@@ -7,8 +7,8 @@
           <div class="bg-clip-border rounded-xl bg-white text-gray-700 shadow-md lg:h-max lg:scale-105 z-10 translate-y-0">
             <div class="p-6 text-center">
               <NuxtImg provider="cloudinary" src="/register.jpg" class="object-scale-down w-full h-auto mx-auto" />
-              <p class="mt-4">🏋️‍♀️ Lifters unite! Membership registration and renewals for 2024 are now open! Join us as we gear up for another powerful year of strength and determination. 💪</p>
-              <NuxtLink to="https://forms.gle/gXT5hMf2U3obvGxJ8">
+              <p class="mt-4">🏋️‍♀️ Lifters unite! Membership registration and renewals for 2025 are now open! Join us as we gear up for another powerful year of strength and determination. 💪</p>
+              <NuxtLink to="https://docs.google.com/forms/d/1kpXFQGTxzxB7KB-nnCnWD7u-w-dtEtw8tVzpKU67D5I">
                 <button class="mt-4 w-full align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none flex items-center justify-center gap-4" type="button">
                   register
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="w-5 h-5">
