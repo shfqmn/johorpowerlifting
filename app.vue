6031674d1cd6fe2c033e718c0ce56b1b9a28655b
shfqmn <prof.shafiq@gmail.com>
2025-07-06 04:14:47 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -21,11 +21,10 @@ useHead({
 </script>
 
 <template>
-  <div class="w-full h-[80vh]">
-    <iframe
-      src="https://sites.google.com/view/johorpowerlifting"
-      class="w-full h-full border-0"
-      allowfullscreen
-    ></iframe>
-  </div>
+  <Body class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-950">
+    <NuxtLayout>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </NuxtLayout>
+  </Body>
 </template>
